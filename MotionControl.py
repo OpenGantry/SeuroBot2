4c484ddfb84b1b10916378cd82f64ada1de3c543
niko1499 <nxgamarra@gmail.com>
2019-10-12 14:55:10 -0500

    Revert "initial commit"
    This reverts commit af742b213566e8bb878d9dfd7fd3f2864112b87c.

--- MotionControl.py
+++
@@ -1,9 +0,0 @@
-#from adafruit_motorkit import MotorKit
-
-from Adafruit_CircuitPython_MotorKit import adafruit_motorkit
-from Adafruit_CircuitPython_MotorKit.adafruit_motorkit import MotorKit
-
-kit = MotorKit()
-
-for i in range(100):
-    kit.stepper1.onestep()
