4242cc59fda1d12318379db8a4b0f34570093e4e
niko1499 <nxgamarra@gmail.com>
2019-10-12 19:57:50 -0500

    test4

--- StepTest4.py
+++ StepTest4.py
@@ -6,27 +6,32 @@ import time
 
 kit = MotorKit(0x6f)
 
-while True:
+NotDone = True
+
+while NotDone:
     print("Single coil steps")
-    for i in range(100):
+    for i in range(200):
         kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
-    for i in range(100):
+    for i in range(200):
         kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
-
+    time.sleep(8)
     print("Double coil steps")
-    for i in range(100):
+    for i in range(200):
         kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
-    for i in range(100):
+    for i in range(200):
         kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-
+    time.sleep(8)
     print("Interleaved coil steps")
-    for i in range(100):
+    for i in range(200):
         kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.INTERLEAVE)
-    for i in range(100):
+    for i in range(200):
         kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.INTERLEAVE)
-
+    time.sleep(8)
     print("Microsteps")
-    for i in range(100):
+    for i in range(200):
         kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.MICROSTEP)
-    for i in range(100):
+    for i in range(200):
         kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.MICROSTEP)
+
+    notDone = False
+
