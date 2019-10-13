ecaaab7cd8b53383f859d85ebcb916465473ca89
niko1499 <nxgamarra@gmail.com>
2019-10-12 19:49:44 -0500

    test4

---
+++ StepTest4.py
@@ -0,0 +1,32 @@
+"""Simple test for using adafruit_motorkit with a stepper motor"""
+from adafruit_motorkit import MotorKit
+from adafruit_motor import MotorKit
+
+import time
+
+kit = MotorKit(0x6f)
+
+while True:
+    print("Single coil steps")
+    for i in range(100):
+        kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
+    for i in range(100):
+        kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
+
+    print("Double coil steps")
+    for i in range(100):
+        kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
+    for i in range(100):
+        kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+
+    print("Interleaved coil steps")
+    for i in range(100):
+        kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.INTERLEAVE)
+    for i in range(100):
+        kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.INTERLEAVE)
+
+    print("Microsteps")
+    for i in range(100):
+        kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.MICROSTEP)
+    for i in range(100):
+        kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.MICROSTEP)
