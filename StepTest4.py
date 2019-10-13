ed6ec06407cbeac299590359020ffab43248e1c3
niko1499 <nxgamarra@gmail.com>
2019-10-12 19:54:36 -0500

    lib and step fixes

--- StepTest4.py
+++ StepTest4.py
@@ -1,6 +1,6 @@
 """Simple test for using adafruit_motorkit with a stepper motor"""
+from adafruit_motor import stepper
 from adafruit_motorkit import MotorKit
-from adafruit_motor import MotorKit
 
 import time
 
@@ -9,24 +9,24 @@ kit = MotorKit(0x6f)
 while True:
     print("Single coil steps")
     for i in range(100):
-        kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
+        kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.SINGLE)
     for i in range(100):
-        kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
+        kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.SINGLE)
 
     print("Double coil steps")
     for i in range(100):
-        kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
+        kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
     for i in range(100):
-        kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+        kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
 
     print("Interleaved coil steps")
     for i in range(100):
-        kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.INTERLEAVE)
+        kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.INTERLEAVE)
     for i in range(100):
-        kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.INTERLEAVE)
+        kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.INTERLEAVE)
 
     print("Microsteps")
     for i in range(100):
-        kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.MICROSTEP)
+        kit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.MICROSTEP)
     for i in range(100):
-        kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.MICROSTEP)
+        kit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.MICROSTEP)
