100c4825ece0b6e4fdabe7ba3d6f456ea793a8c4
niko1499 <nxgamarra@gmail.com>
2019-10-27 16:50:03 -0500

    motor test

---
+++ MotorTest2.py
@@ -0,0 +1,17 @@
+"""Simple test for using adafruit_motorkit with a stepper motor"""
+from adafruit_motor import stepper
+from adafruit_motorkit import MotorKit
+
+import time
+
+kit = MotorKit(0x6f)
+
+NotDone = True
+myRange=200
+delay=10
+
+
+
+kit.motor2.throttle = 1.0
+time.sleep(delay)
+kit.motor2.throttle = 0
